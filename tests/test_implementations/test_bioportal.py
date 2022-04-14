e9b63c41cc094b7455f7509f87240aca817d79d8
Patrick Kalita <pkalita@lbl.gov>
2022-04-14 12:06:33 -0700

    Add initial implementation of get_sssom_mappings_by_curie in bioportal_implementation

--- tests/test_implementations/test_bioportal.py
+++ tests/test_implementations/test_bioportal.py
@@ -2,32 +2,36 @@ import itertools
 import logging
 import unittest
 
+from linkml_runtime.dumpers import yaml_dumper
 from oaklib.implementations.bioportal.bioportal_implementation import BioportalImplementation
 
-from tests import HUMAN, NEURON
+from tests import DIGIT, HUMAN, NEURON
 
 class TestBioportal(unittest.TestCase):
 
     def setUp(self) -> None:
         oi = BioportalImplementation()
-        self.has_apikey = True
         try:
             oi.load_bioportal_api_key()
         except ValueError:
-            logging.info('Skipping bioportal tests, no API key set')
-            self.has_apikey = False
+            self.skipTest('Skipping bioportal tests, no API key set')
         self.oi = oi
 
     def test_text_annotator(self):
-        if self.has_apikey:
-            results = list(self.oi.annotate_text('hippocampal neuron from human'))
-            for ann in results:
-                logging.info(ann)
-            assert any(r for r in results if r.object_id == HUMAN)
-            assert any(r for r in results if r.object_id == NEURON)
+        results = list(self.oi.annotate_text('hippocampal neuron from human'))
+        for ann in results:
+            logging.info(ann)
+        assert any(r for r in results if r.object_id == HUMAN)
+        assert any(r for r in results if r.object_id == NEURON)
 
 
     def test_search(self):
-        if self.has_apikey:
-            results = list(itertools.islice(self.oi.basic_search('tentacle pocket'), 20))
-            assert 'CEPH:0000259' in results
+        results = list(itertools.islice(self.oi.basic_search('tentacle pocket'), 20))
+        assert 'CEPH:0000259' in results
+
+
+    def test_mappings(self):
+        mappings = list(self.oi.get_sssom_mappings_by_curie(DIGIT))
+        for m in mappings:
+            print(yaml_dumper.dumps(m))
+        assert any(m for m in mappings if m.object_id == 'http://purl.obolibrary.org/obo/NCIT_C73791')
