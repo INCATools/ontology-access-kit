f5c400bac5c41d72f289ae70f3f66d23ec8ce032
cmungall <cjm@berkeleybop.org>
2022-04-15 17:38:08 -0700

    fixing TC examples for GCI

--- tests/test_utilities/test_taxon_constraints.py
+++ tests/test_utilities/test_taxon_constraints.py
@@ -11,7 +11,8 @@ from oaklib.utilities.subsets.subset_analysis import all_subsets_overlap, compar
 from oaklib.utilities.taxon.taxon_constraint_utils import nr_term_taxon_constraints_simple, all_term_taxon_constraints, \
     get_term_with_taxon_constraints
 
-from tests import OUTPUT_DIR, INPUT_DIR, INTRACELLULAR, CELLULAR_ORGANISMS
+from tests import OUTPUT_DIR, INPUT_DIR, INTRACELLULAR, CELLULAR_ORGANISMS, PHOTOSYNTHETIC_MEMBRANE, \
+    PLANTS_OR_CYANOBACTERIA
 from tests.test_cli import NUCLEUS, NUCLEAR_ENVELOPE, BACTERIA, EUKARYOTA
 
 DB = INPUT_DIR / 'go-nucleus.db'
@@ -25,15 +26,6 @@ class TestTaxonConstraintsUtils(unittest.TestCase):
     def setUp(self) -> None:
         oi = ProntoImplementation(OntologyResource(slug=str(TEST_ONT), local=True))
         self.oi = oi
-
-    def test_one(self):
-        oi = self.oi
-        t = 'GO:0005622'
-        never, only = all_term_taxon_constraints(oi, t)
-        never_nr, only_nr = nr_term_taxon_constraints_simple(oi, t)
-        self.assertCountEqual([EUKARYOTA, 'NCBITaxon:131567'], only)
-        self.assertCountEqual([EUKARYOTA], only_nr)
-        self.assertIn('NCBITaxon:131567', list(oi.ancestors('NCBITaxon:2759', predicates=[IS_A])))
 
     def test_never_in(self):
         oi = self.oi
@@ -50,22 +42,63 @@ class TestTaxonConstraintsUtils(unittest.TestCase):
         assert not only.redundant_with_only_in
         assert only.taxon.id == EUKARYOTA
         assert only.redundant_with == []
+        # See: https://github.com/althonos/pronto/issues/163
 
 
-    def test_with_datamodel(self):
+    def test_intracellular(self):
         oi = self.oi
         t = INTRACELLULAR
         st = get_term_with_taxon_constraints(oi, t, include_redundant=True)
-        #print(f'T: {yaml_dumper.dumps(st)}')
+        self.assertCountEqual(st.never_in, [])
+        self.assertEqual(1, len(st.only_in))
         never = [tc.taxon.id for tc in st.never_in]
         only = [tc.taxon.id for tc in st.only_in]
         never_nr = [tc.taxon.id for tc in st.never_in if not tc.redundant]
         only_nr = [tc.taxon.id for tc in st.only_in if not tc.redundant]
         never_r = [tc.taxon.id for tc in st.never_in if tc.redundant]
         only_r = [tc.taxon.id for tc in st.only_in if tc.redundant]
+        self.assertCountEqual([CELLULAR_ORGANISMS], only)
+        self.assertCountEqual([CELLULAR_ORGANISMS], only_nr)
+        self.assertCountEqual([], only_r)
+
+    def test_photosynthetic_membrane(self):
+        oi = self.oi
+        t = PHOTOSYNTHETIC_MEMBRANE
+        st = get_term_with_taxon_constraints(oi, t, include_redundant=True)
+        print(yaml_dumper.dumps(st))
+        self.assertCountEqual(st.never_in, [])
+        self.assertEqual(2, len(st.only_in))
+        never = [tc.taxon.id for tc in st.never_in]
+        only = [tc.taxon.id for tc in st.only_in]
+        never_nr = [tc.taxon.id for tc in st.never_in if not tc.redundant]
+        only_nr = [tc.taxon.id for tc in st.only_in if not tc.redundant]
+        never_r = [tc.taxon.id for tc in st.never_in if tc.redundant]
+        only_r = [tc.taxon.id for tc in st.only_in if tc.redundant]
+        self.assertCountEqual([PLANTS_OR_CYANOBACTERIA, CELLULAR_ORGANISMS], only)
+        self.assertCountEqual([PLANTS_OR_CYANOBACTERIA], only_nr)
+        self.assertCountEqual([CELLULAR_ORGANISMS], only_r)
+
+    def test_nuclear_envelope(self):
+        oi = self.oi
+        t = NUCLEAR_ENVELOPE
+        st = get_term_with_taxon_constraints(oi, t, include_redundant=True)
+        print(yaml_dumper.dumps(st))
+        self.assertEqual(1, len(st.never_in))
+        self.assertEqual(2, len(st.only_in))
+        never = [tc.taxon.id for tc in st.never_in]
+        only = [tc.taxon.id for tc in st.only_in]
+        never_nr = [tc.taxon.id for tc in st.never_in if not tc.redundant]
+        only_nr = [tc.taxon.id for tc in st.only_in if not tc.redundant]
+        never_r = [tc.taxon.id for tc in st.never_in if tc.redundant]
+        only_r = [tc.taxon.id for tc in st.only_in if tc.redundant]
+        self.assertCountEqual([BACTERIA], never)
+        self.assertCountEqual([BACTERIA], never_nr)
+        self.assertCountEqual([], never_r)
+        assert st.never_in[0].redundant_with_only_in
         self.assertCountEqual([EUKARYOTA, CELLULAR_ORGANISMS], only)
         self.assertCountEqual([EUKARYOTA], only_nr)
         self.assertCountEqual([CELLULAR_ORGANISMS], only_r)
+
 
     def test_taxon_subclass(self):
         self.assertIn(CELLULAR_ORGANISMS, list(self.oi.ancestors(BACTERIA, predicates=[IS_A])))
