0679712ca3989e04fbc3aa6f32183d92b7fdad49
Harshad Hegde <hegdehb@gmail.com>
2022-07-07 10:23:38 -0500

    unnecessary imports

--- src/oaklib/implementations/bioportal/agroportal_implementation.py
+++ src/oaklib/implementations/bioportal/agroportal_implementation.py
@@ -4,10 +4,6 @@ from oaklib.implementations import BioportalImplementation
 from oaklib.interfaces.mapping_provider_interface import MappingProviderInterface
 from oaklib.interfaces.search_interface import SearchInterface
 from oaklib.interfaces.text_annotator_interface import TextAnnotatorInterface
-from oaklib.types import CURIE, URI
-from oaklib.utilities.apikey_manager import get_apikey_value
-from oaklib.utilities.rate_limiter import check_limit
-from sssom_schema import Mapping
 
 @dataclass
 class AgroportalImplementation(
