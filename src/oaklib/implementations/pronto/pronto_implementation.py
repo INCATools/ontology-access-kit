3016c545842b80006cff3a7c93a223e55ae20f60
Harshad Hegde <hegdehb@gmail.com>
2022-08-03 08:55:21 -0500

    reformatted

--- src/oaklib/implementations/pronto/pronto_implementation.py
+++ src/oaklib/implementations/pronto/pronto_implementation.py
@@ -521,7 +521,7 @@ class ProntoImplementation(
                     syn.description = patch.new_value
         elif isinstance(patch, kgcl.NewSynonym):
             t = self._entity(patch.about_node, strict=True)
-            # Get scope from patch.qualifier 
+            # Get scope from patch.qualifier
             # rather than forcing all synonyms to be related.
             scope = str(patch.qualifier.value).upper() if patch.qualifier else "RELATED"
             t.add_synonym(description=patch.new_value, scope=scope)
