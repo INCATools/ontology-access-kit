763c51ee7c2543330de5705dccfc7638bcfb6696
Harshad Hegde <hegdehb@gmail.com>
2022-09-08 11:30:31 -0500

    flkae8 ed

--- src/oaklib/utilities/mapping/cross_ontology_diffs.py
+++ src/oaklib/utilities/mapping/cross_ontology_diffs.py
@@ -19,7 +19,7 @@ from oaklib.utilities.graph.networkx_bridge import mappings_to_graph
 from oaklib.utilities.label_utilities import add_labels_to_object
 
 try:
-    from collections import Iterator, defaultdict
+    from collections import Iterator, defaultdict  # noqa:F811
 except ImportError:  # python versions >= 3.10
     from collections import defaultdict
     from collections.abc import Iterator
