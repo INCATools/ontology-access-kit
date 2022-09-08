8c4f9da9af425c11d663f8421671b0d41cc07abd
Harshad Hegde <hegdehb@gmail.com>
2022-09-08 11:23:06 -0500

    try - catch for conditional collections import

--- src/oaklib/utilities/mapping/cross_ontology_diffs.py
+++ src/oaklib/utilities/mapping/cross_ontology_diffs.py
@@ -18,6 +18,12 @@ from oaklib.types import CURIE, PRED_CURIE
 from oaklib.utilities.graph.networkx_bridge import mappings_to_graph
 from oaklib.utilities.label_utilities import add_labels_to_object
 
+try:
+    from collections import Iterator, defaultdict
+except ImportError:  # python versions >= 3.10
+    from collections import defaultdict
+    from collections.abc import Iterator
+
 ONE_TO_ZERO = MappingCardinalityEnum(MappingCardinalityEnum["1:0"])
 ONE_TO_MANY = MappingCardinalityEnum(MappingCardinalityEnum["1:n"])
 
