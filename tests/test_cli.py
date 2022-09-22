1b213b9591a80776bfed5f89fc3073a9870a6db2
Harshad Hegde <hegdehb@gmail.com>
2022-09-21 19:25:11 -0500

    test for synonymized

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -450,6 +450,8 @@ class TestCommandLineInterface(unittest.TestCase):
             contents = "\n".join(stream.readlines())
             self.assertIn("skos:closeMatch", contents)
             self.assertIn("skos:exactMatch", contents)
+            # TODO change below once mapping_justification for this is finalized.
+            self.assertIn("SYNONYMIZED", contents)
         self.assertEqual("", err)
 
     def test_lexmatch_sqlite(self):
