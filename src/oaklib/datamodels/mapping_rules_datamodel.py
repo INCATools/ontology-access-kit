5fd565233dbeb820add004e5be3ac2fbe311d3db
Harshad Hegde <hegdehb@gmail.com>
2022-10-18 10:34:03 -0500

    re made python file

--- src/oaklib/datamodels/mapping_rules_datamodel.py
+++ src/oaklib/datamodels/mapping_rules_datamodel.py
@@ -1,5 +1,5 @@
 # Auto generated from mapping_rules_datamodel.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-10-18T08:25:52
+# Generation date: 2022-10-18T10:32:30
 # Schema: mapping-rules
 #
 # id: https://w3id.org/linkml/mapping_rules_datamodel
