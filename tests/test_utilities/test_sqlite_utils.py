18e5ddd6f591d0d01f0cccd2ba6c2173b86dd183
David Linke <dalito@users.noreply.github.com>
2022-11-08 19:50:37 +0100

    Better testing for sqlite_bulk_load (#347)
    * Better testing for sqlite_bulk_load
    
    * Comment spelling fix
    
    * Don't fail CI for coverage data upload problem

--- tests/test_utilities/test_sqlite_utils.py
+++ tests/test_utilities/test_sqlite_utils.py
@@ -19,15 +19,21 @@ class TestSqliteUtils(unittest.TestCase):
         sqlite_bulk_load(DB, TSV, TBL_NAME, cols=["a", "b", "c"])
         con = sqlite3.connect(str(DB))
         rows = list(con.execute(f"SELECT * FROM {TBL_NAME}"))
-        self.assertGreater(len(rows), 5)
+        self.assertEqual(len(rows), 16)
+        # Check first row which could be interpreted as column names.
         self.assertIn(("MGI", "MGI:1918911", "0610005C13Rik"), rows)
+        # last row
+        self.assertIn(("MGI", "MGI:3698435", "0610009E02Rik"), rows)
 
     def test_chunked_bulkload(self):
         if DB.exists():
             DB.unlink()
-        args = dict(chunksize=1000, sep="\t", comment="!", names=list("abc"))
+        args = dict(chunksize=10, sep="\t", comment="!", names=list("abc"))
         sqlite_bulk_load2(DB, TSV, TBL_NAME, read_csv_args=args)
         con = sqlite3.connect(str(DB))
         rows = list(con.execute(f"SELECT * FROM {TBL_NAME}"))
-        self.assertGreater(len(rows), 5)
+        self.assertEqual(len(rows), 16)
+        # first row from first chunk
         self.assertIn(("MGI", "MGI:1918911", "0610005C13Rik"), rows)
+        # last row from 2nd (and last) chunk
+        self.assertIn(("MGI", "MGI:3698435", "0610009E02Rik"), rows)
