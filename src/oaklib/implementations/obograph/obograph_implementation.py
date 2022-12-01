80dfac48b8762fccbb09c292f84cbb639a419359
cmungall <cjm@berkeleybop.org>
2022-11-30 17:06:31 -0800

    Adding a valueset toolkit, performs expansion.
    Fixes #370

--- src/oaklib/implementations/obograph/obograph_implementation.py
+++ src/oaklib/implementations/obograph/obograph_implementation.py
@@ -40,6 +40,7 @@ from oaklib.interfaces.basic_ontology_interface import (
     RELATIONSHIP_MAP,
 )
 from oaklib.interfaces.differ_interface import DifferInterface
+from oaklib.interfaces.dumper_interface import DumperInterface
 from oaklib.interfaces.mapping_provider_interface import MappingProviderInterface
 from oaklib.interfaces.obograph_interface import OboGraphInterface
 from oaklib.interfaces.patcher_interface import PatcherInterface
@@ -68,6 +69,7 @@ class OboGraphImplementation(
     SearchInterface,
     MappingProviderInterface,
     PatcherInterface,
+    DumperInterface,
 ):
     """
     OBO Graphs JSON backed implementation.
@@ -377,13 +379,13 @@ class OboGraphImplementation(
 
     def dump(self, path: str = None, syntax: str = "json"):
         logging.info(f"Dumping graph to {path} syntax: {syntax}")
-        if syntax in RDFLIB_FORMAT_MAP:
-            converter = OboGraphToRdfOwlConverter(curie_converter=self.converter)
-            g = converter.convert(self.obograph_document)
-            g.serialize(path, format=RDFLIB_FORMAT_MAP[syntax])
+        if syntax == "json" or syntax == "obojson":
+            if path is None:
+                print(json_dumper.dumps(self.obograph_document))
+            else:
+                json_dumper.dump(self.obograph_document, to_file=str(path))
         else:
-            logging.info("Using JSON dumper")
-            json_dumper.dump(self.obograph_document, to_file=str(path))
+            super().dump(path, syntax)
 
     def save(
         self,
