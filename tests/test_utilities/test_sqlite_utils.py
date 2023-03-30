835f6b4130ebe4eb71af913213e0f13f5154c50b
Harshad Hegde <hegdehb@gmail.com>
2023-03-29 21:58:25 -0500

    change permissions

--- tests/test_utilities/test_sqlite_utils.py
+++ tests/test_utilities/test_sqlite_utils.py
@@ -1,6 +1,7 @@
 import sqlite3
+import stat
 import unittest
-import shutil
+import os
 
 from oaklib.implementations.sqldb.sqlite_utils import (
     sqlite_bulk_load,
@@ -16,8 +17,8 @@ TBL_NAME = "my_tbl"
 class TestSqliteUtils(unittest.TestCase):
     def test_bulkload(self):
         if DB.exists():
-            shutil.rmtree(DB)
-            # DB.unlink()
+            os.chmod(DB , stat.S_IWRITE)
+            DB.unlink()
         sqlite_bulk_load(DB, TSV, TBL_NAME, cols=["a", "b", "c"])
         con = sqlite3.connect(str(DB))
         rows = list(con.execute(f"SELECT * FROM {TBL_NAME}"))
@@ -30,6 +31,7 @@ class TestSqliteUtils(unittest.TestCase):
 
     def test_chunked_bulkload(self):
         if DB.exists():
+            os.chmod(DB , stat.S_IWRITE)
             DB.unlink()
         args = dict(chunksize=10, sep="\t", comment="!", names=list("abc"))
         sqlite_bulk_load2(DB, TSV, TBL_NAME, read_csv_args=args)
