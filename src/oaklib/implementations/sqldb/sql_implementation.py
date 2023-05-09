84afde700c2112fc0017f69eb0cfe6568d0478b3
Harshad Hegde <hegdehb@gmail.com>
2023-05-09 16:21:22 -0500

    `simple_mappings_by_curie` => `sssom_mappings`

--- src/oaklib/implementations/sqldb/sql_implementation.py
+++ src/oaklib/implementations/sqldb/sql_implementation.py
@@ -1051,10 +1051,12 @@ class SqlImplementation(
             raise ValueError(f"Node {curie} does not exist")
 
     def simple_mappings_by_curie(self, curie: CURIE) -> Iterable[Tuple[PRED_CURIE, CURIE]]:
-        for row in self.session.query(HasMappingStatement).filter(
-            HasMappingStatement.subject == curie
-        ):
-            yield row.predicate, row.value
+        # for row in self.session.query(HasMappingStatement).filter(
+        #     HasMappingStatement.subject == curie
+        # ):
+        #     yield row.predicate, row.value
+        for x in self.sssom_mappings(curies=[curie]):
+            yield x.predicate_id, x.object_id
 
     def query(
         self, query: str, syntax: str = None, prefixes: List[str] = None, **kwargs
