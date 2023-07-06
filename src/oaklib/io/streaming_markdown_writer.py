55d863b3b5af1580f58f0be0a6abc94bee63c5a6
Harshad Hegde <hegdehb@gmail.com>
2023-07-06 10:57:36 -0500

    formatted

--- src/oaklib/io/streaming_markdown_writer.py
+++ src/oaklib/io/streaming_markdown_writer.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass
 from typing import Any
 
+from kgcl_schema.datamodel.kgcl import (
+    ChangeLanguageElement,
+    EdgeChange,
+    NodeChange,
+    NodeMove,
+)
 from linkml_runtime import CurieNamespace
 
 from oaklib.interfaces.obograph_interface import OboGraphInterface
 from oaklib.interfaces.taxon_constraint_interface import TaxonConstraintInterface
 from oaklib.io.streaming_writer import StreamingWriter
 from oaklib.utilities.obograph_utils import DEFAULT_PREDICATE_CODE_MAP
-from kgcl_schema.datamodel.kgcl import ChangeLanguageElement, NodeChange, EdgeChange, NodeMove
 
 predicate_code_map = DEFAULT_PREDICATE_CODE_MAP
 
@@ -38,12 +43,13 @@ class StreamingMarkdownWriter(StreamingWriter):
 
             elif isinstance(curie, EdgeChange):
                 change = curie
-                # !Here we only have only SPO e.g.: subject='PATO:0045073', predicate='RO:0015010', object='PATO:0000461'
+                # !Here we only have only SPO e.g.: 
+                # ! subject='PATO:0045073', predicate='RO:0015010', object='PATO:0000461'
                 # !It needs to have old and new values to show in the release notes.
                 if isinstance(change, NodeMove):
                     # ! Here we have old_value but no new_value e.g. old_value='rdfs:subPropertyOf', new_value=None
                     curie = change.old_value
-                    
+
                 else:
                     curie = change.predicate
                 # import pdb; pdb.set_trace()
@@ -70,7 +76,11 @@ class StreamingMarkdownWriter(StreamingWriter):
                 self.file.write(f"* {p}\n")
                 for v in vs:
                     self.file.write(f'    * {v} "{oi.label(curie)}"\n')
-        if self.display_options and "t" in self.display_options and isinstance(oi, TaxonConstraintInterface):
+        if (
+            self.display_options
+            and "t" in self.display_options
+            and isinstance(oi, TaxonConstraintInterface)
+        ):
             self.file.write("### Taxon Constraints\n\n")
             tc_subj = oi.get_term_with_taxon_constraints(curie)
             for tc in tc_subj.never_in:
