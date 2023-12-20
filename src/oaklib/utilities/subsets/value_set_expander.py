83fa6508038dc284b08d441c7d86b2e95ee7637a
Parker Gleason-Bremer <pgleasonbremer@altoslabs.com>
2023-12-19 18:34:48 -0800

    linting

--- src/oaklib/utilities/subsets/value_set_expander.py
+++ src/oaklib/utilities/subsets/value_set_expander.py
@@ -184,7 +184,7 @@ class ValueSetExpander(BasicOntologyInterface, ABC):
     ) -> PermissibleValue:
         definition = oi.definition(curie)
         # \n can break some downstream tooling like LinkML's gen-pydantic (v1.6.6)
-        definition = definition.replace('\n',' ')
+        definition = definition.replace("\n", " ")
         label = oi.label(curie)
         pv_formula = enum_definition.pv_formula if enum_definition else None
         if str(pv_formula) == "CURIE":
