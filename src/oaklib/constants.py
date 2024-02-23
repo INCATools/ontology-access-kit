86d80538a75da46cbdeee4615ccac7d5a719cadb
Harshad Hegde <hegdehb@gmail.com>
2024-02-22 23:34:45 -0600

    constants handled

--- src/oaklib/constants.py
+++ src/oaklib/constants.py
@@ -12,10 +12,6 @@ NODE_RENAME = "NodeRename"
 CLASS_CREATION = "ClassCreation"
 NODE_CREATION = "NodeCreation"
 NODE_DELETION = "NodeDeletion"
-
 NODE_TEXT_DEFINITION_CHANGE = "NodeTextDefinitionChange"
 NEW_TEXT_DEFINITION = "NewTextDefinition"
-NODE_UNOBSOLETION = "NodeUnobsoletion"
-NODE_DIRECT_MERGE = "NodeDirectMerge"
-NODE_OBSOLETION_WITH_DIRECT_REPLACEMENT = "NodeObsoletionWithDirectReplacement"
-NODE_OBSOLETION = "NodeObsoletion"
+
