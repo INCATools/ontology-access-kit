d3803e772fecd12963bcd7907d24b6822f316a8a
Harshad Hegde <hegdehb@gmail.com>
2024-03-11 11:33:25 -0500

    cleanup+formatted

--- src/oaklib/implementations/semsimian/semsimian_implementation.py
+++ src/oaklib/implementations/semsimian/semsimian_implementation.py
@@ -4,17 +4,7 @@ import inspect
 import logging
 import math
 from dataclasses import dataclass, field
-from typing import (
-    TYPE_CHECKING,
-    ClassVar,
-    Dict,
-    Iterable,
-    Iterator,
-    List,
-    Optional,
-    Tuple,
-    Union,
-)
+from typing import ClassVar, Dict, Iterable, Iterator, List, Optional, Tuple, Union
 
 from oaklib.datamodels.similarity import (
     BestMatch,
