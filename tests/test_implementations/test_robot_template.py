1b216c9092366e26a0e166e5674ffc00d696915b
Chris Mungall <cjm@berkeleybop.org>
2024-03-20 17:44:17 -0700

    Adding metadata to asserts

--- tests/test_implementations/test_robot_template.py
+++ tests/test_implementations/test_robot_template.py
@@ -75,12 +75,12 @@ def test_set_label(adapter):
     if not isinstance(adapter, PatcherInterface):
         raise ValueError("Adapter does not support patching")
     adapter.set_label(BRAIN_SPECIMEN, "FOO")
-    assert adapter.label(BRAIN_SPECIMEN) == "FOO"
+    assert adapter.label(BRAIN_SPECIMEN) == "FOO", "failed to update in-memory"
     if not isinstance(adapter, DumperInterface):
         raise ValueError("Adapter does not support dumping")
     adapter.dump(SAVED_TEMPLATES, clean=True)
     adapter2 = get_adapter(f"robottemplate:{SAVED_TEMPLATES}")
-    assert adapter2.label(BRAIN_SPECIMEN) == "FOO"
+    assert adapter2.label(BRAIN_SPECIMEN) == "FOO", "failed to update on disk"
 
 
 @pytest.mark.parametrize(
