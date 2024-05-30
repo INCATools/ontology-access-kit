f5be1bc66337045b0a5aa6e1f6f39a444758032b
Chris Mungall <cjm@berkeleybop.org>
2024-05-30 16:10:42 -0700

    adding dissector

--- src/oaklib/utilities/axioms/logical_definition_utilities.py
+++ src/oaklib/utilities/axioms/logical_definition_utilities.py
@@ -1,7 +1,9 @@
 from typing import List, Optional
 
+from oaklib import BasicOntologyInterface
 from oaklib.datamodels.obograph import LogicalDefinitionAxiom
 from oaklib.datamodels.vocabulary import IS_A
+from oaklib.interfaces import OboGraphInterface
 from oaklib.types import CURIE, PRED_CURIE
 
 
@@ -57,3 +59,28 @@ def logical_definition_matches(
         if ldef.definedClassId not in subjects:
             return False
     return True
+
+
+def roll_up_to_genus(term: CURIE, adapter: BasicOntologyInterface, predicates: List[PRED_CURIE]) -> CURIE:
+    """
+    Walks up the genus hierarchy.
+
+    This can be used to extract the "meaningful" genus of a compositional term
+
+    :param term:
+    :param adapter:
+    :param predicates:
+    :return:
+    """
+    if not isinstance(adapter, OboGraphInterface):
+        raise NotImplemented
+    replacement = None
+    for ldef in adapter.logical_definitions([term]):
+        if len(ldef.restrictions) == 1 and len(ldef.genusIds) == 1:
+            restr = ldef.restrictions[0]
+            if restr.propertyId in predicates:
+                replacement = ldef.genusIds[0]
+    if replacement:
+        return roll_up_to_genus(replacement, adapter, predicates)
+    else:
+        return term
