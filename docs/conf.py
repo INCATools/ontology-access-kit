58114c7fbb97b35f3bba86463c54cc8f79bead2f
Daniel Korn <6885702+DnlRKorn@users.noreply.github.com>
2026-03-24 16:18:39 -0400

    Fixed D100 and I001 rule violations in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,10 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+Configuration file for the Sphinx documentation builder.
+
+ This file only contains a selection of the most common options. For a full
+ list see the documentation:
+ https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 # -- Path setup --------------------------------------------------------------
 
@@ -14,7 +16,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 from datetime import date
-
 
 # -- Project information -----------------------------------------------------
 
